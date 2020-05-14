5acac9fdc587c8931c98818114201d8772afa2a9
Ayshah Aljedani <50632806+ayshahAljedani@users.noreply.github.com>
2020-05-14 02:22:18 -0400

    Add files via upload

---
+++ git-lab-program.cpp
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ayshah Aljedani>
+ *        Date: <14.5.20>
+ * Description: print Hello git. 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
